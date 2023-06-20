5afc5b3a74bd22c5ff367598bd23a94dfb6c5748
fiska <fiska@FISKAL>
2023-06-20 15:06:18 -0300

    Adicionar arquivos de projeto.

---
+++ BotRpaChallenge/ExcelUtils.cs
@@ -0,0 +1,43 @@
+﻿using ExcelDataReader;
+using System.Data;
+
+
+namespace BotRpaChallenge
+{
+    public class ExcelUtils
+    {
+        public static readonly int IndexCabecarioExcel = 0; 
+        public static List<Pessoa> LerExcel(string diretorioArquivo)
+        {
+            IExcelDataReader reader;
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            FileStream stream = File.Open(diretorioArquivo, FileMode.Open, FileAccess.Read);
+            reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            System.Data.DataSet result = reader.AsDataSet();
+            try
+            {
+                List<Pessoa> listaPessoas = new();
+                foreach (DataRow linha in result.Tables[0].Rows)
+                {
+                    Pessoa pessoa = new()
+                    {
+                        FirstName = (string)linha["Column0"],
+                        LastName = (string)linha["Column1"],
+                        CompanyName = (string)linha["Column2"],
+                        Role = (string)linha["Column3"],
+                        Address = (string)linha["Column4"],
+                        Email = (string)linha["Column5"],
+                        PhoneNumber = linha["Column6"].ToString()
+                    };
+                    listaPessoas.Add(pessoa);
+                }
+                listaPessoas.RemoveAt(IndexCabecarioExcel);
+                return listaPessoas;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
---
+++ BotRpaChallenge/Program.cs
@@ -0,0 +1,52 @@
+﻿using BotRpaChallenge;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+class Program
+{
+    static readonly string downloadsFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+    static readonly string filePath = Path.Combine(downloadsFolderPath, "challenge.xlsx");
+    static void Main(string[] args)
+    {
+        IWebDriver driver = new ChromeDriver
+        {
+            Url = "https://www.rpachallenge.com"
+        };
+        string botaoBaixarExcel = "/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/a";
+        bool fileExists = File.Exists(filePath);
+        if (!fileExists)
+        {
+            driver.FindElement(By.XPath(botaoBaixarExcel)).Click();
+            Thread.Sleep(1000);
+        }
+        var listaPessoas = ExcelUtils.LerExcel(filePath);
+        string botaoStart = "/html/body/app-root/div[2]/app-rpa1/div/div[1]/div[6]/button";
+        driver.FindElement(By.XPath(botaoStart)).Click();
+
+        InputarDadosPessoa(driver, listaPessoas);
+        SalvarResultado(driver);
+
+        Thread.Sleep(1000);
+        driver.Quit();
+    }
+
+    private static void InputarDadosPessoa(IWebDriver driver, List<Pessoa> listaPessoas)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelFirstName']")).SendKeys(listaPessoas[i].FirstName);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelLastName']")).SendKeys(listaPessoas[i].LastName);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelCompanyName']")).SendKeys(listaPessoas[i].CompanyName);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelRole']")).SendKeys(listaPessoas[i].Role);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelAddress']")).SendKeys(listaPessoas[i].Address);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelEmail']")).SendKeys(listaPessoas[i].Email);
+            driver.FindElement(By.XPath("//*[@ng-reflect-name='labelPhone']")).SendKeys(listaPessoas[i].PhoneNumber);
+            driver.FindElement(By.XPath("/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input")).Click();
+        }
+    }
+    private static void SalvarResultado(IWebDriver driver)
+    {
+        string conteudo = driver.FindElement(By.XPath("/html/body/app-root/div[2]/app-rpa1/div/div[2]")).Text;
+        System.IO.File.WriteAllText(downloadsFolderPath+"\\resultado.txt", conteudo);
+    }
+}
